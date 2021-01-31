7997e8d994d2588cf2f6f1a7903682913c0efe20
RemoteSoul-Hub <yahya.tahoune@gmail.com>
2021-01-31 23:44:49 +0100

    Toggle effect, media queries, onclick listener

---
+++ main.js
@@ -0,0 +1,16 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // Toggle Nav
+
+    burger.addEventListener('click',()=> {
+        nav.classList.toggle('nav-active');
+    });
+    // Animate Toggle
+    navLinks.forEach((link, index)=> {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+    });
+}
+
+navSlide();
